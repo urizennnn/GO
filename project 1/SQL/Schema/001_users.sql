79236e072246b51b7be590602117e96695556f18
urizennnnnn <igamerryt@gmail.com>
2024-01-22 15:17:46 +0100

    setting up database

---
+++ project 1/SQL/Schema/001_users.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+
+    CREATE TABLE users (
+        id UUID PRIMARY KEY
+        created_at TIMESTAMP NOT NULL
+        updated_at TIMESTAMP NOT NULL
+        name text not null
+    );
+
+
+-- +goose Down
+
+drop tables users;
