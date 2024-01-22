a1dc5307770abdac191d4b0227f91d5e6612da62
urizennnnnn <igamerryt@gmail.com>
2024-01-22 13:14:03 +0100

    first commit

---
+++ project 1/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1router := chi.NewRouter()
+
+	v1router.Get("/ready", handlerReadines)
+	router.Mount("/v1", v1router)
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in .env")
+	}
+	log.Printf("Server starting on port %v", portString)
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	server.ListenAndServe()
+
+	fmt.Println("PORT:", portString)
+}
